2b138bcd65f762bb81b5969467a131c91b590f58
SSHI <suchen.shi.2022@outlook.com>
2024-05-22 23:48:49 -0400

    Auto-generated commit

---
+++ server/chunk-SRSQIGQU.mjs
@@ -0,0 +1,3 @@
+import './polyfills.server.mjs';
+import{a as p,b as r,c as i,d as n,e as m,l as s,n as a,o as f,r as c,x as l}from"./chunk-IEPFXLTA.mjs";var d=(()=>{let t=class t{constructor(){this.title="GameDemo3"}};t.\u0275fac=function(e){return new(e||t)},t.\u0275cmp=p({type:t,selectors:[["app-root"]],standalone:!0,features:[m],decls:5,vars:0,template:function(e,y){e&1&&(r(0,"h1"),n(1,` Hello world
+`),i(),r(2,"div")(3,"p"),n(4," This is my First Angular app. "),i()())}});let o=t;return o})();var u=[];var v={providers:[l(u),f()]};var C={providers:[c()]},g=s(v,C);var h=()=>a(d,g),k=h;export{k as a};
