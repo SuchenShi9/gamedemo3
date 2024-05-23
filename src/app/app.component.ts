8655e5e0cc45bb920aab8274aeb1d856adca615c
SSHI <suchen.shi.2022@outlook.com>
2024-05-23 19:52:58 -0400

    11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatRadioModule } from '@angular/material/radio';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatButtonModule, MatSlideToggleModule, MatRadioModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/directory/directory.component.ts
+++ src/app/components/directory/directory.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatRadioButton } from '@angular/material/radio';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-directory',
   standalone: true,
-  imports: [],
+  imports: [MatSlideToggleModule, MatRadioButton],
   templateUrl: './directory.component.html',
   styleUrl: './directory.component.css'
 })
