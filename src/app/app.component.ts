a80b1b6d9c6d52510098bb01fdf3752f45b20058
SSHI <suchen.shi.2022@outlook.com>
2024-05-23 12:56:06 -0400

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppRoutingModule, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { DirectoryComponent } from './components/directory/directory.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'directory', component: DirectoryComponent },
+    { path: 'contact_us', component: ContactComponent },
+    { path: 'about', component: AboutComponent },
+    { path: '', redirectTo: 'directory', pathMatch: 'full' },
+];
