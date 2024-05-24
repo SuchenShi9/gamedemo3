8534c3c1b69cea6391ebd082fd92f34e1481217d
SSHI <suchen.shi.2022@outlook.com>
2024-05-24 14:12:59 -0400

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
+import { MatButtonModule, MatIconButton } from '@angular/material/button';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatRadioModule } from '@angular/material/radio';
-
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { FlexLayoutModule } from "@angular/flex-layout";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatButtonModule, MatSlideToggleModule, MatRadioModule],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, FlexLayoutModule, MatButtonModule, MatSlideToggleModule, MatRadioModule, CommonModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/playground/colorcard/colorcard.component.ts
+++ src/app/components/playground/colorcard/colorcard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Card } from './card.class';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-colorcard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './colorcard.component.html',
   styleUrl: './colorcard.component.css'
 })
