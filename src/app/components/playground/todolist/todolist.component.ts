ec7cb6417b7302d793548e6d1833e341fddbddb7
SSHI <suchen.shi.2022@outlook.com>
2024-05-24 17:17:53 -0400

    1

--- src/app/components/playground/todolist/todolist.component.ts
+++ src/app/components/playground/todolist/todolist.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-todolist',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './todolist.component.html',
   styleUrl: './todolist.component.css'
 })
-export class TodolistComponent {
+export class TodolistComponent implements OnInit {
+  userInput: string = '';
+  todoList: { name: string, completed: boolean }[] = [{ name: 'Task 1', completed: false }, { name: 'Task 2', completed: true }, { name: 'Task 3', completed: false }, { name: 'Task 4', completed: true }];
+  incompleteCount: number = this.todoList.length;
 
+  constructor() { }
+  ngOnInit(): void {
+    this.incompleteCount = this.todoList.filter((todo) => !todo.completed).length;
+  }
+
+  addTodo() {
+    if (this.userInput != '') {
+      this.todoList.push({ name: this.userInput, completed: false });
+      this.incompleteCount = this.todoList.filter((todo) => !todo.completed).length;
+      this.userInput = '';
+    }
+  }
+
+  toggleComplete(index: number) {
+    this.todoList[index].completed = !this.todoList[index].completed;
+    this.incompleteCount = this.todoList.filter((todo) => !todo.completed).length;
+  }
 }
