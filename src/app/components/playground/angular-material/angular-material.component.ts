b07d62017f54694a0bcbc44d09b2b70548acadbf
SSHI <suchen.shi.2022@outlook.com>
2024-06-06 17:49:25 -0400

    update angular material

--- src/app/components/playground/angular-material/angular-material.component.ts
+++ src/app/components/playground/angular-material/angular-material.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import {
+  MatDialog,
+  MatDialogRef,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogTitle,
+  MatDialogContent,
+} from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-angular-material',
   standalone: true,
-  imports: [],
-  templateUrl: './angular-material.component.html',
-  styleUrl: './angular-material.component.css'
+  imports: [MatButtonModule, MatFormFieldModule, MatInputModule, MatDatepickerModule],
+  providers: [provideNativeDateAdapter()],
+  templateUrl: 'angular-material.component.html',
+  styleUrl: 'angular-material.component.css'
 })
 export class AngularMaterialComponent {
+  constructor(public dialog: MatDialog) { }
 
+  openDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
+    this.dialog.open(DialogAnimationsExampleDialog, {
+      width: '250px',
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+  }
+}
+
+
+@Component({
+  selector: 'dialog-animations-example-dialog',
+  templateUrl: 'dialog-animations-example-dialog.html',
+  standalone: true,
+  imports: [MatButtonModule, MatDialogActions, MatDialogClose, MatDialogTitle, MatDialogContent],
+})
+export class DialogAnimationsExampleDialog {
+  constructor(public dialogRef: MatDialogRef<DialogAnimationsExampleDialog>) { }
 }
